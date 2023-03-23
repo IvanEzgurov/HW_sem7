1620203bbf2972ee170159a2691344a6a51a41cd
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-03-23 18:42:25 +0300

    task3_52

---
+++ task3_52/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите количество строк m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк n: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write(array[i, j] + " ");
+
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        avarage = (avarage + array[i, j]);
+    }
+    avarage = avarage/m;
+    Console.Write(avarage + "; ");
+}
